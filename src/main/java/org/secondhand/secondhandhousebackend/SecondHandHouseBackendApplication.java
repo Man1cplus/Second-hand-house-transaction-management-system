81390f3a2f20262effc156141320221db07da27e
zdqqqqqqqqq <1813845185@qq.com>
2025-06-24 15:07:17 +0800

    后端项目初始化

---
+++ src/main/java/org/secondhand/secondhandhousebackend/SecondHandHouseBackendApplication.java
@@ -0,0 +1,13 @@
+package org.secondhand.secondhandhousebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondHandHouseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondHandHouseBackendApplication.class, args);
+    }
+
+}
